7a4669cced950d55c34eab03401b0b04c8c0a7c9
정재현 <chwogus0303@naver.com>
2024-05-29 00:44:05 +0900

    초기 커밋.

---
+++ Algorithm/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// Improve the input/output speed of cin, cout
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cout << "Hello World" << "\n";
+
+	return 0;
+}
