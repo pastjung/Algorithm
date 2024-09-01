edd26742fd9c4cd3eab81e6ba580e262d2d36096
정재현 <87860163+pastjung@users.noreply.github.com>
2024-09-01 14:30:05 +0900

    [Bronze I] Title: 알고리즘 수업 - 선택 정렬 1, Time: 112 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Bronze/23881. 알고리즘 수업 － 선택 정렬 1/알고리즘 수업 － 선택 정렬 1.cc
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void selection_sort(vector<int> arr, int K) {
+	int count = 0;
+	for (int i = arr.size() - 1; i > 0; i--) {
+		// 최대값 탐색
+		int last = i;
+		for (int j = 0; j < i; j++) {
+			if (arr[j] > arr[last]) {
+				last = j;
+			}
+		}
+
+		// 최대값 우측에 배치
+		if (last != i) {
+			int temp = arr[i];
+			arr[i] = arr[last];
+			arr[last] = temp;
+			count++;
+		}
+
+		if (count == K) {
+			cout << arr[last] << ' ' << arr[i] << "\n";
+			break;
+		}
+	}
+
+	if (count < K) {
+		cout << -1 << "\n";
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N, K;
+	cin >> N >> K;
+
+	vector<int> arr;
+	for (int i = 0; i < N; i++) {
+		int A;
+		cin >> A;
+		arr.push_back(A);
+	}
+
+	selection_sort(arr, K);
+}
