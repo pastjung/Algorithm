fbb9aee3634fb3ace306b74e44d80474954bd794
정재현 <87860163+pastjung@users.noreply.github.com>
2024-11-15 11:34:50 +0900

    [D3] Title: 햄버거 다이어트, Time: 1,674 ms, Memory: 13,544 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.cpp
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.cpp
@@ -6,7 +6,13 @@ using namespace std;
 int func(vector<pair<int, int>> arr, int N, int L){
  	int max_taste_score = 0;
     
-    // 가능한 모든 비트마스크 조합 탐색 ( 1 << N = 2^N )
+    // 가능한 모든 비트마스크 조합 탐색
+    /*
+    	각 숫자는 비트로 표시했을 때 선택/비선택을 하는 방식으로 해석
+        ex) bitmask = 5 -> 101 -> 첫 번째와 세 번째 재료를 선택 O, 두 번째 재료는 선택 X
+        이때  1 << N == 2^N
+        시간 복잡도 : O(2*N * N ) -> 이 방법은 재료의 수가 많지 않은 문제에서 적합 & N이 커지면 시간이 급격하게 증가
+    */
     for (int bitmask = 0; bitmask < (1 << N); bitmask++){
      	int total_taste = 0;
         int total_calories = 0;
