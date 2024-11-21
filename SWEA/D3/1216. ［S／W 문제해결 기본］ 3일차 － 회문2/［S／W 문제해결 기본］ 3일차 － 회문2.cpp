1cf588a1d62d94e3c947fc1be81520c70deb105c
정재현 <87860163+pastjung@users.noreply.github.com>
2024-11-21 23:53:10 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문2, Time: 427 ms, Memory: 13,680 KB -BaekjoonHub

--- SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.cpp
+++ SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<string>
+#include<cmath>
 using namespace std;
 
 bool check(string str, int N, int index){
@@ -10,36 +11,29 @@ bool check(string str, int N, int index){
     return true;
 } 
 
-int findMax(string str, int preMax){
-    int max = 1;
-    for(int len = 100; len > preMax; len--){
+int findMax(string str, int preMaxLen){
+    int maxLen = 1;
+    for(int len = 100; len > preMaxLen; len--){
         for(int index = 0; index <= 100 - len; index++){
-            if(check(str, len, index)) if(max < len) max = len;
+            if(check(str, len, index)) maxLen = max(maxLen, len);
         }
     }
- 	return max;   
+ 	return maxLen;   
 }
 
 int func(vector<string> row) {
-    int max = 1;
+    int maxLen = 1;
     vector<string> column(100, "");
+    
     for(int i = 0; i < 100; i++){
         for(int j = 0; j < 100; j++){
             column[j] += row[i][j];
         }
     }
     
-    for(string str : row){
-     	int len = findMax(str, max);
-        if(max < len) max = len;
-    }
-    
-    for(string str : column){
-     	int len = findMax(str, max);
-        if(max < len) max = len;
-    }
-    
-    return max;
+    for(string str : row) maxLen = max(maxLen, findMax(str, maxLen));
+    for(string str : column) maxLen = max(maxLen, findMax(str, maxLen));
+    return maxLen;
 }
 
 int main(){
