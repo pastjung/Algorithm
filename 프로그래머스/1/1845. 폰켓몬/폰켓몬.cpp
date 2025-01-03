8f4bcd8338a8940341660b835aab1760b2120322
정재현 <87860163+pastjung@users.noreply.github.com>
2025-01-03 16:03:02 +0900

    [level 1] Title: 폰켓몬, Time: 0.91 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -1,14 +1,13 @@
 #include <vector>
-#include <set>
+#include <unordered_set>
 using namespace std;
 
-// key : value = 번호 : 순서
 int solution(vector<int> nums)
 {
     int answer = 0;    
     int N = nums.size() / 2;
     
-    set<int> isNums;
+    unordered_set<int> isNums;
     for(int i = 0; i < nums.size(); i++){
         isNums.insert(nums[i]);
     }
