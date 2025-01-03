ba17788998db3bad11f2b391ca69b16cb9179fa6
정재현 <87860163+pastjung@users.noreply.github.com>
2025-01-03 16:31:34 +0900

    [level 2] Title: 전화번호 목록, Time: 313.95 ms, Memory: 49.2 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -1,19 +1,29 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <unordered_set>
 
 using namespace std;
 
 bool solution(vector<string> phone_book) {
     bool answer = true;
+    unordered_set<string> strSet;
     
-    stable_sort(phone_book.begin(), phone_book.end());
+    // Hash Table 생성
+    for(string str : phone_book){
+        strSet.insert(str);
+    }
     
-    for(int i = 0; i < phone_book.size() - 1; i++){
-        if(phone_book[i + 1].find(phone_book[i]) == 0){
-            answer = false;
-            break;
+    // 각 전화번호가 다른 전화번호의 접두사인지 확인
+    for(string phoneNum : phone_book){
+        string preNum = "";
+        for(int i = 0; i < phoneNum.size(); i++){
+            preNum += phoneNum[i];
+            if(strSet.find(preNum) != strSet.end() && preNum != phoneNum){
+                answer = false;
+                break;
+            }
         }
+        
     }
     
     return answer;
