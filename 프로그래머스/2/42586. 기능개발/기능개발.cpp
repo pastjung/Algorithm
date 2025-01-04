0c0517d222968d4dd16147914f416b4459327838
정재현 <87860163+pastjung@users.noreply.github.com>
2025-01-04 15:19:37 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -4,20 +4,20 @@ using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
+    
+    int max_day = 0;    // 작업의 배포 기준 관리
+    for (int idx = 0; idx < progresses.size(); idx++){
+        int day = (99 - progresses[idx]) / speeds[idx] + 1; // 작업 완료 일수
 
-    int day;
-    int max_day = 0;
-    for (int i = 0; i < progresses.size(); ++i)
-    {
-        day = (99 - progresses[i]) / speeds[i] + 1;
-
+        // 배포 그룹 관리
         if (answer.empty() || max_day < day)
             answer.push_back(1);
-        else
-            ++answer.back();
-
-        if (max_day < day)
+        else{
+            answer.back()++;    
+        }            
+        if (max_day < day){
             max_day = day;
+        }
     }
 
     return answer;
