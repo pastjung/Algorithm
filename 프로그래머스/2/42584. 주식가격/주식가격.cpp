f827fe084dbfe2dacece78b165e6f320b2474313
정재현 <87860163+pastjung@users.noreply.github.com>
2025-01-05 21:43:37 +0900

    [level 2] Title: 주식가격, Time: 25.96 ms, Memory: 26.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.cpp
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -1,19 +1,29 @@
 #include <string>
 #include <vector>
+#include <stack>
 
 using namespace std;
 
 vector<int> solution(vector<int> prices) {
-    vector<int> answer(prices.size(), 0);
+    vector<int> answer(prices.size());
+    stack<int> priceStack;
+    int N = prices.size();
     
-    for(int i = 0; i < prices.size() - 1; i++){
-        for(int j = i + 1; j < prices.size(); j++){
-            answer[i]++;
-            if(prices[i] > prices[j]){
-                break;
-                
-            } 
+    for(int idx = 0; idx < N; idx++){
+        // 가격이 큰 순서로 스택에 삽입 -> top보다 작은 값을 만날 경우 pop()
+        while(!priceStack.empty() && prices[priceStack.top()] > prices[idx]){
+            answer[priceStack.top()] = idx - priceStack.top();
+            priceStack.pop();
         }
+        
+        // 현재 값 삽입
+        priceStack.push(idx);
+    }
+    
+    // 모든 값 비교 후 남은 값들 시간 계산
+    while(!priceStack.empty()){
+        answer[priceStack.top()] = N - priceStack.top() - 1;
+        priceStack.pop();
     }
     
     return answer;
