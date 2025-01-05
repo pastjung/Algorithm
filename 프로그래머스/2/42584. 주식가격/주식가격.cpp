24365f4c98b129db3de9579b066f2e521929aa95
정재현 <87860163+pastjung@users.noreply.github.com>
2025-01-05 21:32:58 +0900

    [level 2] Title: 주식가격, Time: 27.69 ms, Memory: 26.7 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.cpp
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -7,13 +7,12 @@ vector<int> solution(vector<int> prices) {
     vector<int> answer(prices.size(), 0);
     
     for(int i = 0; i < prices.size() - 1; i++){
-        answer[i]++;
-        for(int j = i + 1; j < prices.size() - 1; j++){
-            if(prices[i] <= prices[j]){
-                answer[i]++;
-            } else {
+        for(int j = i + 1; j < prices.size(); j++){
+            answer[i]++;
+            if(prices[i] > prices[j]){
                 break;
-            }
+                
+            } 
         }
     }
     
