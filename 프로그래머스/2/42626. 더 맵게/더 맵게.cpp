7d77c6c66ae06522e47551171c542c60e4757bc4
정재현 <87860163+pastjung@users.noreply.github.com>
2025-01-06 12:02:56 +0900

    [level 2] Title: 더 맵게, Time: 233.14 ms, Memory: 41.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include<iostream>
+
+using namespace std;
+
+int solution(vector<int> scoville, int K) {
+    int answer = 0;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    
+    for(int s : scoville){
+        minHeap.push(s);
+    }
+    
+    while(minHeap.top() < K){
+        if(minHeap.size() == 1){
+            answer = -1;
+            break;
+        }
+        
+        int min = minHeap.top();
+        minHeap.pop();
+        int secondMin = minHeap.top();
+        minHeap.pop();
+        
+        minHeap.push(min + ( 2 * secondMin ));
+        answer++;
+    }
+    
+    return answer;
+    
+}
