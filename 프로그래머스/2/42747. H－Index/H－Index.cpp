f77e8f546281afbed272774dbbfa129f6ad370d8
정재현 <87860163+pastjung@users.noreply.github.com>
2025-01-07 14:16:13 +0900

    [level 2] Title: H-Index, Time: 0.06 ms, Memory: 4.23 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.cpp
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int solution(vector<int> citations) {
     int answer = 0;
     
-    sort(citations.begin(), citations.end(), greater<>());
+    sort(citations.begin(), citations.end(), greater<>());  // 내림차순 정렬
     
     for(int i = 0; i < citations.size(); i++){
         if(citations[i] >= i + 1){
