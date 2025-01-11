72c625f525efad98aadd7b54e7753c92c756c2ae
정재현 <87860163+pastjung@users.noreply.github.com>
2025-01-11 22:03:58 +0900

    [level 2] Title: 피로도, Time: 22.19 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.cpp
+++ 프로그래머스/2/87946. 피로도/피로도.cpp
@@ -1,29 +1,27 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
+void dfs(int index, int k, vector<vector<int>> dungeons, vector<bool> visited, int& answer) {
+    if (index > answer) {
+        answer = index;
+    }
+    
+    for (int i = 0; i < dungeons.size(); i++) {
+        if (!visited[i] && dungeons[i][0] <= k) {
+            visited[i] = true;
+            dfs(index + 1, k - dungeons[i][1], dungeons, visited, answer);
+            visited[i] = false;
+        }
+    }
+}
+
 int solution(int k, vector<vector<int>> dungeons) {
     int answer = 0;
-    
-    sort(dungeons.begin(), dungeons.end());
-    do {
-		    int current_k = k;    // 현재 체력
-		    int explore_num = 0;  // 탐험한 던전의 수
+    vector<bool> visited(dungeons.size(), false);
 
-		    for(vector<int> dungeon : dungeons) {
-				    if(dungeon[0] <= current_k){
-						    explore_num++;
-						    current_k -= dungeon[1];
-				    }
-		    }
-		    
-		    if(explore_num > answer){
-				    answer = explore_num;
-		    }
-		    
-    } while(next_permutation(dungeons.begin(), dungeons.end()));
+    dfs(0, k, dungeons, visited, answer);
     
     return answer;
 }
