bda84e0d2f719391cadc1fa8aa5e26a6d62a417a
정재현 <87860163+pastjung@users.noreply.github.com>
2025-01-12 10:32:13 +0900

    [level 2] Title: 모음 사전, Time: 0.01 ms, Memory: 4.05 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
@@ -1,37 +1,13 @@
 #include <string>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
-void Backtracking(string alpha, string current, vector<string>& dictionary){
-    if(current.length() > 5){
-        return;
-    }
-    
-    if(!current.empty()){
-        dictionary.push_back(current);
-    }
-    
-    for (int i = 0; i < alpha.size(); i++) {
-        Backtracking(alpha, current + alpha[i], dictionary);
-    }
-}
-
 int solution(string word) {
-    int answer = 0;
-    
-    vector<string> dictionary;
-    string alpha = "AEIOU";
-    
-    // 모든 단어 사전 생성
-    Backtracking(alpha, "", dictionary);
-    
-    // 사전 순으로 정렬
-    sort(dictionary.begin(), dictionary.end());
-    
-    auto it = find(dictionary.begin(), dictionary.end(), word);
-    answer = distance(dictionary.begin(), it) + 1;
-    
+    string alpha = string("AEIOU");
+    int answer = word.size();
+
+    for(int i = 0, weight = 1; i < word.size(); i++, weight *= 5)
+        answer += alpha.find(word[i]) * 781 / weight;
+
     return answer;
 }
