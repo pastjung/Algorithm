8e1b07c8efdfdec43a0c744abd34c9dcc2bc6dab
정재현 <87860163+pastjung@users.noreply.github.com>
2025-02-19 23:29:20 +0900

    [level 2] Title: 타겟 넘버, Time: 10.55 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -3,19 +3,23 @@
 
 using namespace std;
 
-void DFS(vector<int>& numbers, int target, int number, int idx, int& answer){
-    if(idx == numbers.size()){
-        if(target == number) {
-            answer++;
-        }
-        return;
-    } 
-    DFS(numbers, target, number + numbers[idx], idx + 1, answer);
-    DFS(numbers, target, number - numbers[idx], idx + 1, answer);
-}
-
 int solution(vector<int> numbers, int target) {
     int answer = 0;
-    DFS(numbers, target, 0, 0, answer);
+    int size = 1 << numbers.size();
+    
+    for(int i = 1; i < size; i++) {
+        int temp = 0;
+        for(int j = 0; j < numbers.size(); j++) {
+            if(i & (1 << j)) {
+                temp -= numbers[j];
+            } else {
+                temp += numbers[j];
+            }
+        }
+        if(temp == target) {
+            answer++;
+        }
+    }
+    
     return answer;
 }
