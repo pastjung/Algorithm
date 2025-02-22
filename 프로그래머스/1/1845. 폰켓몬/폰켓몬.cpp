87b60a8273d5f3ac7500de02e85c097b249b68cd
정재현 <87860163+pastjung@users.noreply.github.com>
2025-02-22 10:20:15 +0900

    [level 1] Title: 폰켓몬, Time: 0.72 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -4,19 +4,19 @@ using namespace std;
 
 int solution(vector<int> nums)
 {
-    int answer = 0;    
+    int answer = 0;
     int N = nums.size() / 2;
     
     unordered_set<int> isNums;
-    for(int i = 0; i < nums.size(); i++){
-        isNums.insert(nums[i]);
+    for(int i : nums){
+        isNums.insert(i);
     }
     
     if(isNums.size() <= N){
         answer = isNums.size();
     } else {
         answer = N;
-    }
+    }    
     
     return answer;
 }
