13cdb73c6b375266de29335aca9b4bfc21a1e6ba
정재현 <87860163+pastjung@users.noreply.github.com>
2025-05-07 22:41:30 +0900

    [level 4] Title: 사칙연산, Time: 3.23 ms, Memory: 3.8 MB -BaekjoonHub

---
+++ 프로그래머스/4/1843. 사칙연산/사칙연산.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+/*
+풀이 방법
+- 구간 [i, j]의 최댓값 또는 최솟값 계산
+- 연산자가 "+"인 경우 : 전체 식의 최댓값을 구하기 위해 -> 부분 식의 최댓값 + 최댓값
+- 연산자가 "-"인 경우 : 전체 식의 최댓값을 구하기 위해 -> 부분 식의 최댓값 - 최솟값
+*/
+int solution(vector<string> arr) {
+    int n = arr.size();
+    int N = (n + 1) / 2;
+    
+    // dp[i][j] : 구간 [i, j]의 최댓값 또는 최솟값
+    vector<vector<int>> dp_max(N, vector<int>(N, INT_MIN));
+    vector<vector<int>> dp_min(N, vector<int>(N, INT_MAX));
+    
+    // 초기화 : 길이가 1인 DP
+    for(int i = 0; i < N; i++){
+        dp_max[i][i] = stoi(arr[i * 2]);
+        dp_min[i][i] = stoi(arr[i * 2]);
+    }
+    
+    // 길이 2 이상인 구간에 대한 DP
+    for(int len = 2; len <= N; len++){
+        for(int i = 0; i <= N - len; i++){
+            int j = i + len - 1;                // i : 구간의 시작, j : 구간의 끝, k : 구간 내 값
+            for(int k = i; k < j; k++){
+                string op = arr[k * 2 + 1];     // op : 연산자
+                if(op == "+"){
+                    dp_max[i][j] = max(dp_max[i][j], dp_max[i][k] + dp_max[k+1][j]);
+                    dp_min[i][j] = min(dp_min[i][j], dp_min[i][k] + dp_min[k+1][j]);
+                } else if(op == "-"){
+                    dp_max[i][j] = max(dp_max[i][j], dp_max[i][k] - dp_min[k+1][j]);
+                    dp_min[i][j] = min(dp_min[i][j], dp_min[i][k] - dp_max[k+1][j]);
+                }
+            }
+        }
+    }
+    
+    return dp_max[0][N - 1];
+}
