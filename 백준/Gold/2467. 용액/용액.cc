9a4b0614de02cfc082fb8142684533e00e871f35
정재현 <87860163+pastjung@users.noreply.github.com>
2025-05-13 21:57:04 +0900

    [Gold V] Title: 용액, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int N;
+    cin >> N;
+
+    vector<int> arr(N, 0);
+    for(int i = 0; i < N; i++) cin >> arr[i];
+    
+    int left = 0, right = N - 1;
+    int l = 0, r = N - 1, minSum = abs(arr[left] + arr[right]);
+    while(left < right){       
+        int sum = arr[left] + arr[right];
+        
+        if(abs(sum) < minSum){
+            l = left;
+            r = right;
+            minSum = abs(sum);
+        }
+
+        if(minSum == 0) break;
+        else if(sum > 0) right--;
+        else left++;
+    }
+    
+    cout << arr[l] << ' ' << arr[r] << "\n";
+    
+    return 0;
+}
