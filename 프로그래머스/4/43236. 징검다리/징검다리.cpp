ca61fee7adc82db42cbf6be77f511dd52221c5d3
정재현 <87860163+pastjung@users.noreply.github.com>
2025-05-13 11:55:46 +0900

    [level 4] Title: 징검다리, Time: 4.14 ms, Memory: 5.19 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.cpp
+++ 프로그래머스/4/43236. 징검다리/징검다리.cpp
@@ -6,19 +6,19 @@ using namespace std;
 
 int solution(int distance, vector<int> rocks, int n) {
     sort(rocks.begin(), rocks.end());
-    rocks.push_back(distance);  // 마지막 지점도 바위로 추가
+    rocks.push_back(distance);      // 마지막 지점도 바위 추가
     
     int left = 1;
-    int right = distance; 
-    int answer = 0;
+    int right = distance;
+    int answer = 0;         // 각 지점 사이의 거리의 최솟값 중 최대값
     
     while(left <= right){
-        int mid = ( left + right ) / 2; // 최소 거리 후보
+        int mid = ( left + right ) / 2;     // 최소 거리 후보
         int prev = 0;
         int removed = 0;
         
         for(int i = 0; i < rocks.size(); i++){
-            if(rocks[i] - prev < mid)   removed++;  // 최소 거리보다 작으면 바위 제거
+            if(rocks[i] - prev < mid) removed++;    // 최소 거리보다 작으면 바위 제거
             else prev = rocks[i];
         }
         
