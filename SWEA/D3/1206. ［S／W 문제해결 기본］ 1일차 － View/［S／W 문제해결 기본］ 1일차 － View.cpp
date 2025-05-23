83ff9363df8d307d15c9de5edfade49708cab0c7
정재현 <87860163+pastjung@users.noreply.github.com>
2025-05-23 15:29:19 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 5 ms, Memory: 5,988 KB -BaekjoonHub

--- SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.cpp
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.cpp
@@ -1,31 +1,35 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-int func(int N){
-    vector<int> arr(N, 0);
-    int count = 0;
- 	for(int i = 0; i < N; i++){
-        cin >> arr[i];
-    }
+int build(vector<int>& building){
+    int cnt = 0;
+    if (building.size() < 5) return 0;
     
-    for(int i = 2; i < N - 2; i++){
-    	int m = max(max(arr[i-2], arr[i-1]), max(arr[i+1], arr[i+2]));
-        if(m < arr[i]) count += arr[i] - m;   
+    for(int i = 2; i < building.size() - 2; i++){
+        int max_neighbor = max({building[i-2], building[i-1], building[i+1], building[i+2]});
+        if(building[i] > max_neighbor){
+            cnt += building[i] - max_neighbor;
+        }
     }
-    return count;
+    return cnt;
 }
 
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
- 	
-    for(int i = 1; i <= 10; i++){
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    for(int t = 1; t <= 10; t++){
         int N;
         cin >> N;
-        cout << '#' << i << ' ' << func(N) << '\n';
+        vector<int> building(N, 0);
+        for(int n = 0; n < N; n++){
+            cin >> building[n];
+        }
+        
+        cout << '#' << t << ' ' << build(building) << '\n';
     }
+    
     return 0;
 }
