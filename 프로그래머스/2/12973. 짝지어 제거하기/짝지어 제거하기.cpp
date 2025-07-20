5d464c0cb038c558b21e960478f598b016c58e07
정재현 <87860163+pastjung@users.noreply.github.com>
2025-07-20 16:53:08 +0900

    [level 2] Title: 짝지어 제거하기, Time: 5.87 ms, Memory: 6.34 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
@@ -0,0 +1,28 @@
+#include<string>
+#include<stack>
+using namespace std;
+
+int solution(string s) {
+    int answer = 0;
+		
+		// 왼쪽 짝을 저장할 stack 선언
+		stack<char> ss;
+		
+		// stack의 top과 s의 char를 비교해서 짝이 있는지 확인
+		for(char c : s){
+			if(ss.empty()){
+				ss.push(c);    // stack이 비어있는 경우 -> push()
+			} else if(ss.top() == c){
+				ss.pop();      // top과 c가 동일한 경우 -> pop()
+			} else{ 
+				ss.push(c);    // top과 c가 다른 경우 -> push()
+			}
+		}
+		
+		// stack이 비어있는 경우 -> answer = 1
+		if(ss.empty()){
+			answer = 1;
+		}
+
+    return answer;
+}
