0639e79dae490e48a02e69cf66428bcc843a1789
정재현 <87860163+pastjung@users.noreply.github.com>
2025-07-20 21:33:50 +0900

    [level 2] Title: 주식가격, Time: 26.13 ms, Memory: 26.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.cpp
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -1,29 +1,46 @@
-#include <string>
-#include <vector>
 #include <stack>
+#include <vector>
 
 using namespace std;
 
 vector<int> solution(vector<int> prices) {
-    vector<int> answer(prices.size());
-    stack<int> priceStack;
-    int N = prices.size();
+    vector<int> answer;
     
-    for(int idx = 0; idx < N; idx++){
-        // 가격이 큰 순서로 스택에 삽입 -> top보다 작은 값을 만날 경우 pop()
-        while(!priceStack.empty() && prices[priceStack.top()] > prices[idx]){
-            answer[priceStack.top()] = idx - priceStack.top();
-            priceStack.pop();
+    // answer 사이즈 설정
+    answer.assign(prices.size(), 0);
+    
+    // 가격이 유지되는지 확인하기 위한 stack 선언 -> {시작 시간, 가격}
+    stack<pair<int, int>> s;
+    
+    // 현재 가격보다 낮은 값이 있는지 stack에서 확인
+    for(int i = 0; i < prices.size(); ++i){
+        // stack의 top()이 현재 값보다 작을경우 반복
+        while(!s.empty() && s.top().second > prices[i]){
+            // stack의 top() 추출
+            auto pair = s.top();
+            s.pop();
+
+            // 떨어지지 않은 시간 계산
+            int time = i - pair.first;
+
+            // 계산된 시간 입력
+            answer[pair.first] = time;
         }
-        
-        // 현재 값 삽입
-        priceStack.push(idx);
+        // 현재 값 스택에 삽입
+        s.push({i, prices[i]});
     }
-    
-    // 모든 값 비교 후 남은 값들 시간 계산
-    while(!priceStack.empty()){
-        answer[priceStack.top()] = N - priceStack.top() - 1;
-        priceStack.pop();
+
+    // stack에 남아있는 값 모두 계산
+    while(!s.empty()){
+        // stack의 top() 추출
+        auto pair = s.top();
+        s.pop();
+
+        // 떨어지지 않은 시간 계산
+        int time = prices.size() - 1 - pair.first;
+
+        // 계산된 시간 입력
+        answer[pair.first] = time;
     }
     
     return answer;
