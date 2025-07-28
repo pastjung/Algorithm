639a7782b088c028457584b0afbfeaff57da2703
정재현 <87860163+pastjung@users.noreply.github.com>
2025-07-28 16:51:33 +0900

    [level 2] Title: 전화번호 목록, Time: 368.18 ms, Memory: 49.2 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -1,31 +1,31 @@
 #include <string>
 #include <vector>
 #include <unordered_set>
+
 using namespace std;
 
 bool solution(vector<string> phone_book) {
     bool answer = true;
+    unordered_set<string> strSet;
     
-    // 해시 테이블 생성
-    unordered_set<string> hash_set;
-    
-    // phone_book의 전화번호를 길이별로 저장 ( 단, 본인은 제외 )
-    for(string phone : phone_book){
-        string num = "";
-        for(char c : phone){
-            num += c;
-            if(phone != num){
-                hash_set.insert(num);   
-            }
-        }
+    // Hash Table 생성
+    for(string str : phone_book){
+        strSet.insert(str);
     }
     
-    // 해시 테이블에 존재할 경우 false리턴
-    for(string phone : phone_book){
-        if(hash_set.find(phone) != hash_set.end()){
-            answer = false;
-            break;
-        }
+    // 각 전화번호가 다른 전화번호의 접두사인지 확인
+    /*
+    풀이 방법 : 각 전화번호의 접두사의 길이를 1부터 점차 늘리면서 해당 접두사가 해시 테이블에 포함되어 있는지 확인
+    */
+    for(string phoneNum : phone_book){
+        string preNum = "";
+        for(int i = 0; i < phoneNum.size(); i++){
+            preNum += phoneNum[i];
+            if(strSet.find(preNum) != strSet.end() && preNum != phoneNum){
+                answer = false;
+                break;
+            }
+        }   
     }
     
     return answer;
