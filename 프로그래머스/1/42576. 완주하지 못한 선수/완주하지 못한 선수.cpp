26755ec532acb9fb1550cd4aec7c942df5eb3a53
정재현 <87860163+pastjung@users.noreply.github.com>
2025-07-28 12:01:16 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 42.77 ms, Memory: 32.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -1,34 +1,34 @@
-#include <string>
 #include <vector>
-#include <unordered_map>
-
+#include <string>
+#include <algorithm>
 using namespace std;
 
 string solution(vector<string> participant, vector<string> completion) {
     string answer = "";
-    unordered_map<string, int> strMap;
     
+    // 해시 테이블 생성
+    unordered_map<string, int> hash_map;
+    
+    // completion 추가
     for(string str : completion){
-        if(strMap.find(str) == strMap.end()){
-            strMap.insert({str, 1});
-        } else{
-            strMap[str]++;
-        }
-        
+        hash_map[str]++;
     }
     
-    // find() : 요소가 존재하면 해댱 요소를, 존재하지 않으면 end() 반복자를 반환
+    // completion에서 participant를 제거
     for(string str : participant){
-        if(strMap.find(str) == strMap.end()){
+        // 해시 테이블에 값이 없는 경우
+        if(hash_map.find(str) == hash_map.end()){
             answer = str;
             break;
-        } else {
-            strMap[str]--;
-            if(strMap[str] < 0){
-                answer = str;
-                break;
-            }
+        }
+        
+        // 해시 테이블의 값이 1인 경우 erase, 1보다 클 경우 ++
+        if(hash_map[str] == 1){
+            hash_map.erase(str);
+        } else{
+            hash_map[str]--;
         }
     }
+    
     return answer;
 }
