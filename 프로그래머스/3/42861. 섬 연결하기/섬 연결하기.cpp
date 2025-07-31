1f81f1d062a873abf320281ec797f6ff61458201
정재현 <87860163+pastjung@users.noreply.github.com>
2025-07-31 13:48:45 +0900

    [level 3] Title: 섬 연결하기, Time: 0.09 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
@@ -1,41 +1,40 @@
+#include <string>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
 
-bool cmp(vector<int> a, vector<int> b) {
+int getParent(vector<int> tree, int node){
+    if(node == tree[node]){
+        return node;
+    } else{
+        return tree[node] = getParent(tree, tree[node]);
+    }
+}
+
+bool cmp(vector<int> a, vector<int> b){
     return a[2] < b[2];
 }
 
 int solution(int n, vector<vector<int>> costs) {
     int answer = 0;
-    int groupSize = 0;
-
-    vector<bool> group(n, false); // 섬이 연결되었는지 여부
-    sort(costs.begin(), costs.end(), cmp); // 비용 기준으로 정렬
-
-    group[costs[0][0]] = true; // 첫 번째 섬 연결
-    group[costs[0][1]] = true;
-    answer += costs[0][2];
-    groupSize = 2;
-
-    while (groupSize < n) {
-        for (int i = 0; i < costs.size(); i++) {
-            int islandA = costs[i][0];
-            int islandB = costs[i][1];
-            int cost = costs[i][2];
-
-            // 하나의 섬만 그룹에 포함된 경우
-            if (group[islandA] ^ group[islandB]) {
-                group[islandA] = true;
-                group[islandB] = true;
-                answer += cost;
-                groupSize++;
-                costs.erase(costs.begin() + i);
-                break;
-            }
+    
+    vector<int> tree(101);
+    for(int i = 0; i < n; i++){
+        tree[i] = i;
+    }
+    
+    sort(costs.begin(), costs.end(), cmp);
+    
+    for(vector<int> cost : costs){
+        int start = getParent(tree, cost[0]);
+        int end = getParent(tree, cost[1]);
+        int weight = cost[2];
+        
+        if(start != end){
+            tree[end] = start;
+            answer += weight;
         }
     }
-
+    
     return answer;
 }
