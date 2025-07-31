d1925c9d8f92c0a291fdc1621c404347ff37acd1
정재현 <87860163+pastjung@users.noreply.github.com>
2025-08-01 00:24:56 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.31 ms, Memory: 4.09 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -2,39 +2,56 @@
 #include<queue>
 using namespace std;
 
-// 아래, 오른쪽, 위쪽, 왼쪽
-int dx[] = {0, 1, 0, -1};
-int dy[] = {1, 0, -1, 0};
+// 방향 벡터
+vector<int> dy = {0, 1, 0, -1};
+vector<int> dx = {1, 0, -1, 0};
 
-// BFS를 활용한 방법
-int solution(vector<vector<int>> maps) {
-    int n = maps.size();       // 행(row) 개수
-    int m = maps[0].size();    // 열(column) 개수
-    queue<vector<int>> q;      // { x, y, length }
-    q.push({0, 0, 1});
+// bfs
+int bfs(vector<vector<int>>& maps, pair<int, int> start, pair<int, int> end){
+    int n = maps.size();
+    int m = maps[0].size();
+    vector<vector<int>> visited(n, vector<int>(m, 0));  // 방문한 노드를 기록할 배열
+    queue<pair<int, int>> q;                            // 인접 노드를 기록할 큐
     
+    // 초기 세팅
+    visited[start.second][start.first] = 1;
+    q.push(start);
+    
+    // 큐를 확인하면서 인접 최단 거리 계산
     while(!q.empty()){
-        int x = q.front()[0];
-        int y = q.front()[1];
-        int length = q.front()[2];
+        // 큐의 front 확인
+        auto [x, y] = q.front();
         q.pop();
         
-        // 목적지 도착하면 경로 길이 반환
-        if (x == n - 1 && y == m - 1) return length;
+        // 현재 위치가 상대 진형인 경우
+        if(x == end.first && y == end.second){
+            return visited[y][x];
+        }
         
-        maps[x][y] = 0;
-        
-        for(int i = 0; i < 4; i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i]; 
+        // 인접 노드 확인
+        for(int dir = 0; dir < 4; dir++){
+            int ny = y + dy[dir];
+            int nx = x + dx[dir];
             
-            // 맵의 범위를 벗어나지 않고, 이동 가능할 때
-            if (0 <= nx && nx < n && 0 <= ny && ny < m && maps[nx][ny] == 1) {
-                maps[nx][ny] = 0;  // 방문 표시 (중복 방지)
-                q.push({nx, ny, length + 1});
+            if(0 <= ny && ny < n && 0 <= nx && nx < m && maps[ny][nx] == 1){
+                if(visited[ny][nx] == 0){
+                    q.push({nx, ny});
+                    visited[ny][nx] = visited[y][x] + 1;
+                } else{
+                    visited[ny][nx] = min(visited[ny][nx], visited[y][x] + 1);    
+                }
             }
         }
     }
     
-    return -1; // 도달 불가능하면 -1 반환
+    // 상대 진형을 가지 못하는 경우
+    return -1;
+}
+
+int solution(vector<vector<int>> maps){
+    pair<int, int> start = {0, 0};                               // 시작 좌표
+    pair<int, int> end = {maps[0].size() - 1, maps.size() -1};   // 목표 좌표
+    
+    // 최단 거리 계산 : bfs
+    return bfs(maps, start, end);
 }
