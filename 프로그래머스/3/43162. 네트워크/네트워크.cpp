de65ddffc1a48cdeda68c991d10976b83fcd024a
정재현 <87860163+pastjung@users.noreply.github.com>
2025-08-01 18:27:27 +0900

    [level 3] Title: 네트워크, Time: 0.06 ms, Memory: 4.07 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -1,39 +1,45 @@
-#include <string>
 #include <vector>
-#include <queue>
-
+#include <unordered_set>
 using namespace std;
 
-void DFS(vector<vector<int>> &graph, vector<bool> &visited, int start){
-    queue<int> q;
-    
-    visited[start] = true;
-    q.push(start);
-    
-    while(!q.empty()){
-        int currentNode = q.front();
-        q.pop();
-        
-        int idx = -1;
-        for(int neighbor : graph[currentNode]){
-            idx++;
-            if(neighbor && !visited[idx]){
-                visited[idx] = true;
-                q.push(idx);
-            }
-        }   
-    }    
+// find 함수 ( 경로 압축 )
+int find(vector<int>& parent, int x){
+    if(parent[x] != x){
+        parent[x] = find(parent, parent[x]);  // 경로 압축
+    }
+    return parent[x];
+}
+
+// unionSet 함수
+void unionSet(vector<int>& parent, int a, int b){
+    int rootA = find(parent, a);
+    int rootB = find(parent, b);
+    if(rootA != rootB){
+        parent[rootB] = rootA;
+    }
 }
 
 int solution(int n, vector<vector<int>> computers) {
-    int answer = 0;
-    vector<bool> visited(n, false);
-    
+    // 부모 배열 생성 및 초기화
+    vector<int> parent(n);
     for(int i = 0; i < n; i++){
-        if(!visited[i]){
-            answer++;
-            DFS(computers, visited, i);
+        parent[i] = i;
+    }
+    
+    // 모든 노드 Union
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(i != j && computers[i][j] == 1){
+                unionSet(parent, i, j);
+            }
         }
     }
-    return answer;
+    
+    // 네트워크 개수 계산
+    unordered_set<int> networks;
+    for(int i = 0; i < n; i++){
+        networks.insert(find(parent, i));
+    }
+    
+    return networks.size();
 }
