15c916ff607f8495faed7fe0e3c622bcc0b53a80
정재현 <87860163+pastjung@users.noreply.github.com>
2025-08-03 22:37:31 +0900

    [level 2] Title: 가장 큰 수, Time: 247.96 ms, Memory: 7.48 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
 
 ### 성능 요약
 
-메모리: 10.5 MB, 시간: 85.68 ms
+메모리: 7.48 MB, 시간: 247.96 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 07일 13:42:53
+2025년 08월 03일 22:37:29
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
@@ -1,31 +1,29 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-
-#include<iostream>
-
 using namespace std;
 
-bool cmp(string a, string b){
-    return a + b > b + a;
+// 정렬 기준
+bool cmp(int a, int b){
+    string s1 = to_string(a);
+    string s2 = to_string(b);
+    return s1 + s2 > s2 + s1;
 }
 
 string solution(vector<int> numbers) {
     string answer = "";
-    vector<string> strNums;
     
-    for(int num : numbers){
-        strNums.push_back(to_string(num));
+    // 두 문자열의 합이 더 큰쪽을 기준으로 정렬
+    sort(numbers.begin(), numbers.end(), cmp);
+    
+    // 정렬된 값으로 문자열 생성
+    for(int number : numbers){
+        answer += to_string(number);
     }
     
-    sort(strNums.begin(), strNums.end(), cmp);
-    
-    for(string num : strNums){
-        answer += num;
-    }
-    
+    // 최종 숫자가 0일 경우
     if(answer[0] == '0'){
-        answer = "0";
+        answer = "0";    
     }
     
     return answer;
