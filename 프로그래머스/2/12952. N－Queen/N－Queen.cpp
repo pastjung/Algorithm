4d193d29e426dffbd032b5d120d2f8090cb999d3
정재현 <87860163+pastjung@users.noreply.github.com>
2025-08-04 01:36:50 +0900

    [level 2] Title: N-Queen, Time: 98.76 ms, Memory: 4.27 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.cpp
+++ 프로그래머스/2/12952. N－Queen/N－Queen.cpp
@@ -3,10 +3,10 @@
 #include <cmath>
 using namespace std;
 
-// 유망 함수
+// 유망 함수 : 현재 (row, col)에 퀸을 놓을 수 있는지 확인
 bool isPromising(vector<int>& board, int row, int col){
     for (int i = 0; i < row; i++) {
-        // 같은 열에 퀸이 있거나, 대각선에 있는 경우
+        // 같은 열 or 같은 대각선에 퀸이 있는 경우
         if (board[i] == col || abs(board[i] - col) == abs(i - row)) {
             return false;
         }
@@ -18,18 +18,18 @@ bool isPromising(vector<int>& board, int row, int col){
 void backtracking(vector<int>& board, int& answer, int row){
     int n = board.size();
     
-    // 기저 조건 : 퀸의 개수가 n개인 경우
+    // 기저 조건 : 모든 행에 퀸을 성공적으로 배치한 경우 ( 해답 1개 완성 )
     if(row == n){
         answer++;
         return;
     }
 
-    // 다음 선택지 탐색 : 가로 내 퀸은 하나만 존재 가능
+    // 다음 선택지 탐색 : 현재 행의 각 열에 대해 퀸을 놓을 수 있는지 검사
     for(int col = 0; col < n; col++){
         // 유망성 검사 : 세로 내 퀸 존재 X && 대각선 내 퀸 존재 X
         if(isPromising(board, row, col)){
             board[row] = col;
-            backtracking(board, answer, row + 1);     // 재귀 호출
+            backtracking(board, answer, row + 1);     // 다음 행으로 재귀 호출
         }
     }
 }
