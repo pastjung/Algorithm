b9977492de0e3e5d50e3e18897cac068d71fa4ff
정재현 <87860163+pastjung@users.noreply.github.com>
2025-08-04 20:59:27 +0900

    [level 2] Title: 피로도, Time: 1.08 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.cpp
+++ 프로그래머스/2/87946. 피로도/피로도.cpp
@@ -1,27 +1,28 @@
 #include <string>
 #include <vector>
-
 using namespace std;
 
-void dfs(int index, int k, vector<vector<int>> dungeons, vector<bool> visited, int& answer) {
-    if (index > answer) {
-        answer = index;
+// 백트래킹
+void backtracking(vector<vector<int>>& dungeons, vector<bool>& visited, int& answer, int k, int count){
+    // 던전의 최대 방문수 갱신
+    if(answer < count){
+        answer = count;
     }
     
-    for (int i = 0; i < dungeons.size(); i++) {
-        if (!visited[i] && dungeons[i][0] <= k) {
-            visited[i] = true;
-            dfs(index + 1, k - dungeons[i][1], dungeons, visited, answer);
-            visited[i] = false;
+    // 다음 던전 탐색
+    for(int i = 0; i < dungeons.size(); i++){
+        // 유망성 검사 : 중복 제거 || 최소 필요도 < 현재 필요도
+        if(!visited[i] && k >= dungeons[i][0]){
+            visited[i] = true;                                                          // 선택 : 해당 던전 탐험
+            backtracking(dungeons, visited, answer, k - dungeons[i][1], count + 1);     // 재귀 호출
+            visited[i] = false;                                                         // 선택 취소
         }
     }
 }
 
 int solution(int k, vector<vector<int>> dungeons) {
     int answer = 0;
-    vector<bool> visited(dungeons.size(), false);
-
-    dfs(0, k, dungeons, visited, answer);
-    
+    vector<bool> visited(dungeons.size(), false);   // 방문 여부를 기록할 배열
+    backtracking(dungeons, visited, answer, k, 0);  // 백트래킹 수행
     return answer;
 }
