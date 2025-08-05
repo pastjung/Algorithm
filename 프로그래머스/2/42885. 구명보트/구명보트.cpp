f62a6398b0b59ad2c7ffd8afb22983ee98d182b8
정재현 <87860163+pastjung@users.noreply.github.com>
2025-08-05 12:01:43 +0900

    [level 2] Title: 구명보트, Time: 1.50 ms, Memory: 4.74 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.cpp
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -1,25 +1,31 @@
-// while 루프를 활용한 방법
 #include <string>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
 
 int solution(vector<int> people, int limit) {
     int answer = 0;
-    int N = people.size();
     
-    // 몸무게를 오름차순으로 정렬
+    // 무게를 기준으로 정렬
     sort(people.begin(), people.end());
     
-    int light = 0; // 가장 가벼운 사람의 인덱스
-    int heavy = N - 1; // 가장 무거운 사람의 인덱스
-    for(; light <= heavy; heavy--){
-        // 가장 가벼운 사람과 가장 무거운 사람의 몸무게 합이 limit 이하인 경우
-        if(people[light] + people[heavy] <= limit){
-            light++;    // 가벼운 사람 탑승
+    int big = people.size() - 1;    // 무게가 높은 쪽의 index
+    int small = 0;                  // 무게가 낮은 쪽의 index
+    
+    // 모든 사람을 구조할 때까지 반복
+    while(small <= big){
+        answer++;
+        
+        // 1명만 남은 경우
+        if(small == big) break;
+        
+        // 무게가 낮은 쪽 사람도 탈 수 있는 경우
+        if(people[small] + people[big] <= limit){
+            small++;
         }
-        answer++;   // 구명보트 1대 사용
+        
+        // 무게가 높은 쪽 태우기
+        big--;
     }
     
     return answer;
