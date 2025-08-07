c5b777fc67c6c8c8f83fe4ae59d725854248d6d3
정재현 <87860163+pastjung@users.noreply.github.com>
2025-08-07 21:03:19 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.cpp
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -1,22 +1,25 @@
 #include <string>
 #include <vector>
-#include <algorithm>
-
-#include <iostream>
-
+#include <cmath>
 using namespace std;
 
-// n * m = brown + yellow
 vector<int> solution(int brown, int yellow) {
     vector<int> answer;
     
-    int sum = brown + yellow;   // 모든 카펫의 개수    
-    for(int i = 3; i < sum / 2; i++) {
-        if(sum % i == 0 && (i - 2) * (sum / i - 2) == yellow) {    
-            answer.push_back(max(i, sum / i));
-            answer.push_back(min(i, sum / i));
-            break;
-        }
+    int total = brown + yellow; // 전체 타일 수
+    
+    // 세로의 길이가 3 ~ sqrt(전체 타일 수) 구간을 확인
+    for(int col = 3; col <= sqrt(total); col++) {
+        // 직사각형을 만들 수 없는 경우 : 전체 타일 수 % 세로 길이 != 0
+        if(total % col != 0) continue;
+           
+        // 카펫을 만들 수 없는 경우 : ((가로 - 1) + (세로 - 1)) * 2 != brown
+        int row = total / col;
+        if(2 * (row + col - 2) != brown) continue;
+        
+        // 카펫을 만든 경우
+        answer = {row, col};
+        break;
     }
     
     return answer;
