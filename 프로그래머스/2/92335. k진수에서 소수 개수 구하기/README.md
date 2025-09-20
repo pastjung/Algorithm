4ceb4b0bda4abe66f444c6f8ab1dd459ee4c0e6e
정재현 <87860163+pastjung@users.noreply.github.com>
2025-09-20 23:40:46 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 8.50 ms, Memory: 4.05 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/README.md
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] k진수에서 소수 개수 구하기 - 92335 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92335) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92335#) 
 
 ### 성능 요약
 
-메모리: 4.21 MB, 시간: 8.94 ms
+메모리: 4.05 MB, 시간: 8.50 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 20일 23:36:05
+2025년 09월 20일 23:40:44
 
 ### 문제 설명
 
--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.cpp
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.cpp
@@ -1,18 +1,7 @@
 #include <string>
 #include <vector>
 #include <cmath>
-#include <algorithm>
 using namespace std;
-
-/* 문제 분석
-- 문제 설명
-    - n을 k진수로 바꿨을 때 변환된 수 안에 소수의 개수 파악
-- 입력 데이터
-    - 양의 정수 n
-    - k 진수
-- 핵심 키워드
-    - 소수 정보 -> 0을 기준으로 분할
-*/
 
 // 소수 판별 함수
 bool isPrime(long long num){
@@ -40,22 +29,19 @@ int solution(int n, int k) {
         
         // 0P0 or P0인 경우
         if(remain == 0){
-            reverse(number.begin(), number.end());
             if(number != "" && isPrime(stoll(number))){
                 answer++;
                 number = "";
             }
         } else {
-            number += to_string(remain);
+            number = to_string(remain) + number;
         }
     }
     // P or 0P 인 경우
     if(number != ""){
-        reverse(number.begin(), number.end());
         if(isPrime(stoll(number))){
             answer++;    
-        }
-        
+        }   
     }
     
     return answer;
