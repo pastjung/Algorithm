c679b668adfe522585493df5611c93aad6d0ca57
정재현 <87860163+pastjung@users.noreply.github.com>
2025-10-02 12:51:25 +0900

    [level 3] Title: 표 편집, Time: 12.97 ms, Memory: 29.8 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.cpp
+++ 프로그래머스/3/81303. 표 편집/표 편집.cpp
@@ -1,70 +1,73 @@
-#include <algorithm>
-#include <stack>
 #include <string>
 #include <vector>
+#include <stack>
 using namespace std;
 
 string solution(int n, int k, vector<string> cmd) {
-    string answer = "";
+    string answer(n, 'O');
     
-    // answer -> n개의 'O'을 가진 문자열로 재할당
-    answer.assign(n, 'O');
+    stack<int> s;               // 삭제된 행을 저장할 스택
+    vector<int> prev(n, 0);     // 이중 연결 리스트 (각 행의 이전 행의 위치를 저장)
+    vector<int> next(n, 0);     // 이중 연결 리스트 (각 행의 다음 행의 위치를 저장)
     
-    // 삭제된 인덱스를 저장할 스택 선언
-    stack<int> deleted;
-    
-    // 각각의 위치에서의 이전 위치와 다음 위치를 저장할 배열 선언
-    vector<int> prev(n), next(n);
-    
-    // 이중 연결 리스트 초기화
-    for (int i = 0; i < n; i++) {
+    // 이중 연결 리스트 초기 설정
+    for(int i = 0; i < n; i++){
         prev[i] = i - 1;
         next[i] = i + 1;
     }
-    next[n - 1] = -1;
     
-    // cmd 명령어 수행
-    for (string c : cmd) {
-        char op = c[0];
-        if (op == 'U') {
-            int x = stoi(c.substr(2));  // 이동할 거리
-            while (x--) {
-                k = prev[k];
+    int cur = k;
+    for(string str : cmd){
+        if(str[0] == 'U'){
+            int move = stoi(str.substr(2));
+            for(int i = 0; i < move; i++){
+                cur = prev[cur];
             }
-        } else if (op == 'D') {
-            int x = stoi(c.substr(2));  // 이동할 거리
-            while (x--) {
-                k = next[k];
+        } else if(str[0] == 'D'){
+            int move = stoi(str.substr(2));
+            for(int i = 0; i < move; i++){
+                cur = next[cur];
             }
-        } else if (op == 'C') {
-            // 현재 위치 삭제
-            deleted.push(k);
-            answer[k] = 'X';
+        } else if(str[0] == 'C'){
+            s.push(cur);
             
-            // 이중 연결 리스트 조정
-            if (prev[k] != -1) {
-                next[prev[k]] = next[k];
+            // 현재 행 삭제
+            if(prev[cur] != -1){
+                next[prev[cur]] = next[cur];
             }
-            if (next[k] != -1) {
-                prev[next[k]] = prev[k];
+            if(next[cur] != n){
+                prev[next[cur]] = prev[cur];   
             }
             
-            // 현재 위치 수정
-            k = (next[k] != -1) ? next[k] : prev[k];
-        } else if (op == 'Z') {
-            // 마지막 삭제 내역 복원
-            int r = deleted.top(); 
-            deleted.pop();
-            answer[r] = 'O';
-            
-            // 이중 연결 리스트 조정
-            if (prev[r] != -1) {
-                next[prev[r]] = r;
+            // 현재 행의 다음 행이 없는 경우 -> 이전 행을 현재 행으로 설정
+            if(next[cur] == n){
+                cur = prev[cur];
+            } else{
+                cur = next[cur];
             }
-            if (next[r] != -1) {
-                prev[next[r]] = r;
+        } else if(str[0] == 'Z'){
+            if(s.empty()){
+                continue;
+            }
+            
+            // stack의 top을 복구
+            int temp = s.top();
+            s.pop();
+            
+            // 이중 연결 리스트 재연결
+            if(prev[temp] != -1){
+                next[prev[temp]] = temp;
+            }
+            if(next[temp] != n){
+                prev[next[temp]] = temp;   
             }
         }
+    }
+    
+    // 삭제된 행 찾기
+    while(!s.empty()){
+        answer[s.top()] = 'X';
+        s.pop();
     }
     
     return answer;
