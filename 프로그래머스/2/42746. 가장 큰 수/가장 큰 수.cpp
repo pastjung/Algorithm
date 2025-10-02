2591f1aedbb73c161a7323b80b0771d22c1ce92b
정재현 <87860163+pastjung@users.noreply.github.com>
2025-10-02 21:27:55 +0900

    [level 2] Title: 가장 큰 수, Time: 261.35 ms, Memory: 7.41 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
@@ -3,27 +3,25 @@
 #include <algorithm>
 using namespace std;
 
-// 정렬 기준
+// 정렬 조건 -> 두 문자열 조합이 더 큰 방향으로 정렬
 bool cmp(int a, int b){
-    string s1 = to_string(a);
-    string s2 = to_string(b);
-    return s1 + s2 > s2 + s1;
+    string A = to_string(a);
+    string B = to_string(b);
+    return A + B > B + A;
 }
 
 string solution(vector<int> numbers) {
     string answer = "";
     
-    // 두 문자열의 합이 더 큰쪽을 기준으로 정렬
     sort(numbers.begin(), numbers.end(), cmp);
     
     // 정렬된 값으로 문자열 생성
-    for(int number : numbers){
-        answer += to_string(number);
+    for(int num : numbers){
+        answer += to_string(num);
     }
     
-    // 최종 숫자가 0일 경우
     if(answer[0] == '0'){
-        answer = "0";    
+        answer = "0";
     }
     
     return answer;
