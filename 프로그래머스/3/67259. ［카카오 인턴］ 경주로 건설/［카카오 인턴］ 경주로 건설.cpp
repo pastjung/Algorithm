769e30b130bde7c46fbd8dfc0b7626e55cdbde89
정재현 <87860163+pastjung@users.noreply.github.com>
2025-10-04 19:32:37 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 0.20 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.cpp
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.cpp
@@ -1,56 +1,69 @@
+#include <string>
 #include <vector>
 #include <queue>
+#include <tuple>
 #include <climits>
 #include <algorithm>
 using namespace std;
 
-// 정보를 저장할 노드
-struct Node {
-    int x, y, cost, dir;
-};
+int SIZE;
+const int STARTX = 0;
+const int STARTY = 0;
+int ENDX;
+int ENDY;
 
-// 방향 벡터
-vector<int> dy = {0, 1, 0, -1};
-vector<int> dx = {1, 0, -1, 0};
+// 방향 벡터 -> 상좌우하
+vector<int> dx = {0, -1, 1, 0};
+vector<int> dy = {1, 0, 0, -1};
 
-// bfs
-int solution(vector<vector<int>> board) {
-    int n = board.size();
-    
-    // 방문 여부를 기록할 배열 - 방향별 방문 기록
-    vector<vector<vector<int>>> visited(n, vector<vector<int>>(n, vector<int>(4, INT_MAX)));   
-    queue<Node> q;   // 인접 노드를 기록할 큐
+// 유망 함수 : 범위 확인
+bool promising(const vector<vector<int>>& board, int x, int y){
+    return STARTX <= x && x <= ENDX && STARTY <= y && y <= ENDY && board[y][x] != 1;
+}
+
+// BFS -> board에 비용 저장
+int BFS(vector<vector<int>> board){
+    queue<tuple<int, int, int>> q;    // 방문한 노드를 저장할 큐 (x, y, dir)
+    vector<vector<vector<int>>> cost(SIZE, vector<vector<int>>(SIZE, vector<int>(4, INT_MAX)));
 
     // 초기 세팅
-    if(board[0][1] == 0){
-        visited[0][1][0] = 100;
-        q.push({1, 0, 100, 0});
+    q.push({STARTX, STARTY, -1});
+    for(int d = 0; d < 4; d++){
+        cost[STARTY][STARTX][d] = 0;
     }
-    if(board[1][0] == 0){
-        visited[1][0][1] = 100;
-        q.push({0, 1, 100, 1});
-    }
-
-    // 인접 노드 관리
+    
+    // 인근 노드 중 방문할 노드가 있는지 확인 (기존 방향, 좌우 방향 확인)
     while(!q.empty()){
-        Node cur = q.front();
+        auto[x, y, dir] = q.front();
         q.pop();
         
-        for(int dir = 0; dir < 4; dir++){
-            int ny = cur.y + dy[dir];
-            int nx = cur.x + dx[dir];
+        for(int ndir = 0; ndir < 4; ndir++){
+            int nx = x + dx[ndir];
+            int ny = y + dy[ndir];
             
-            if(ny < 0 || nx < 0 || n <= ny || n <= nx) continue;
-            if(board[ny][nx] == 1) continue;
+            if(!promising(board, nx, ny)){
+                continue;
+            }
             
-            int newCost = cur.cost + (dir == cur.dir ? 100 : 600);
+            int preCost = (dir == -1) ? 0 : cost[y][x][dir];
+            int newCost = preCost + ((dir == -1 || dir == ndir) ? 100 : 600);
             
-            if(visited[ny][nx][dir] > newCost){
-                visited[ny][nx][dir] = newCost;
-                q.push({nx, ny, newCost, dir});
+            if(newCost < cost[ny][nx][ndir]){
+                cost[ny][nx][ndir] = newCost;
+                q.push({nx, ny, ndir});
             }
         }
     }
     
-    return *min_element(visited[n - 1][n - 1].begin(), visited[n - 1][n - 1].end());
+    return *min_element(cost[ENDY][ENDX].begin(), cost[ENDY][ENDX].end());
+}
+
+int solution(vector<vector<int>> board) {
+    // 전역 변수 초기화
+    SIZE = board.size();
+    ENDX = SIZE - 1;
+    ENDY = SIZE - 1;
+    
+    // BFS 초기화
+    return BFS(board);
 }
