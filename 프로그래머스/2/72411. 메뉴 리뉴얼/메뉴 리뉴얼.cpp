c20329b7fc43fa71e96dd7670e87732b15e998ec
정재현 <87860163+pastjung@users.noreply.github.com>
2025-10-06 22:24:32 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 1.41 ms, Memory: 4.01 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.cpp
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.cpp
@@ -1,56 +1,71 @@
 #include <string>
 #include <vector>
-#include <unordered_map>
+#include <map>
 #include <algorithm>
 using namespace std;
 
-// 조합 생성 함수 ( 문자열, 선택할 메뉴 수 )
-void makeComb(string& order, string comb, int idx, int targetLen, unordered_map<string, int>& courseMap){
-    if (comb.size() == targetLen) {
-        courseMap[comb]++;
+/* 문제 분석
+- 문제 설명
+    - 새로운 메뉴 구성: 단품 조합 -> 코스 요리 형태로 재구성
+    - 각 손님들이 주문할 때 가장 많이 함께 주문한 단품 메뉴들을 코스 요리로 구성
+    - 코스 요리는 최소 2가지 이상 단품 메뉴로 구성
+    - 최소 2명 이상의 손님으로부터 주문된 단품 메뉴 조합에 대해서만 코스 요리 후보에 포함
+- 입력 데이터
+    - 손님이 주문단 단품 메뉴들 orders
+        - 각 원소는 크기가 2 이상 10 이하인 문자열 ( 중복 X )
+    - 추가하고 싶은 코스 요리를 구성하는 단품 메뉴들의 갯수 course
+        - 오름차순
+        - 2 이상 10 이하
+- 핵심 키워드
+    - 각 손님이 단품 메뉴를 2개 이상 주문
+    - n개의 코스 요리 중 가장 많은 주문 횟수를 가진 경우만 저장 ( 동일 개수면 전부 저장 )
+*/
+
+// 주문 조합
+map<string, int> combi;
+
+// nCr 조합 함수
+void combination(string order, string temp, int len){
+    if(temp.size() == len){
+        combi[temp]++;
         return;
     }
-    for (int i = idx; i < order.size(); ++i) {
-        makeComb(order, comb + order[i], i + 1, targetLen, courseMap);
+    
+    for(int i = 0; i < order.size(); i++){
+        combination(order.substr(i+1), temp + order[i], len);
     }
 }
 
 vector<string> solution(vector<string> orders, vector<int> course) {
     vector<string> answer;
     
-    // 코스가 등장할 횟수를 기록할 map 생성 (문자열의 크기별로 따로 저장)
-    unordered_map<string, int> courseMap[11];
-    
-    // 손님 및 course의 개수 별 조합 생성
-    for(string order : orders){
-        // 문자열 정렬
-        sort(order.begin(), order.end());
-        for(int len : course){
-            if (order.size() < len) continue;
-            makeComb(order, "", 0, len, courseMap[len]);
-        }
+    // 주문 정렬
+    for(string& order : orders){
+        sort(order.begin(), order.end());   
     }
     
-    // map의 value가 가장 큰 코스를 answer에 추가 ( 중복 포함 )
-    for(int n : course){
+    // course에 해당하는 길이의 모든 조합 생성
+    for(int len : course){
+        // 각 길이 별 조합 생성 -> 주문 조합에 저장
+        for(string order : orders){
+            combination(order, "", len);
+        }
+    
+        // course에 해당하는 길이의 조합들 중 가장 많은 횟수를 가진 조합 추출
         int maxCount = 0;
-        vector<string> courseCom;
-        for(auto pair : courseMap[n]){
-            if(pair.second >= 2 && pair.second > maxCount){
-                maxCount = pair.second;
-                courseCom.clear();
-                courseCom.push_back(pair.first);
-            } else if(pair.second == maxCount){
-                courseCom.push_back(pair.first);
+        for(auto[str, count] : combi){
+            maxCount = max(maxCount, count);
+        }
+        
+        for(auto[str, count] : combi){
+            if(maxCount >= 2 && maxCount == count){
+                answer.push_back(str);
             }
         }
-        for(string str : courseCom){
-            answer.push_back(str);
-        }
+        
+        combi.clear();
     }
     
-    // answer 오름차순 정렬
     sort(answer.begin(), answer.end());
-    
     return answer;
 }
