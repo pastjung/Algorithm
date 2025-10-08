2a99a24f9f9da4e7fe4d811aa20b49ea1bc46830
정재현 <87860163+pastjung@users.noreply.github.com>
2025-10-08 13:24:36 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 16.88 ms, Memory: 12.7 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.cpp
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.cpp
@@ -4,33 +4,30 @@
 using namespace std;
 
 vector<int> solution(vector<string> gems) {
-    vector<int> answer = {1, (int)gems.size()};
-    int minLength = gems.size();
+    int size = gems.size();
+    vector<int> answer = {1, size};
     
-    // 모든 보석의 종류 파악 -> unordered_map<string, int>
     unordered_map<string, int> jewels;
-    
     for(string gem : gems){
         jewels[gem] = 0;
     }
     
-    // 투포인터: 가장 짧은 구간의 길이 확인
-    int count = 0;
-    for(int start = 0, end = 0; end < gems.size(); end++){
+    int start = 0, kinds = 0;
+    for(int end = 0; end < gems.size(); end++){
         if(jewels[gems[end]] == 0){
-            count++;
+            kinds++;
         }
         jewels[gems[end]]++;
         
-        while(count == jewels.size()){
-            if(end - start + 1 < minLength){
-                answer = {start + 1, end + 1};
-                minLength = end - start + 1;
+        while(jewels.size() == kinds){
+            if(end - start < answer[1] - answer[0]){
+                answer[0] = start + 1;
+                answer[1] = end + 1;
             }
             
             jewels[gems[start]]--;
             if(jewels[gems[start]] == 0){
-                count--;
+                kinds--;
             }
             start++;
         }
