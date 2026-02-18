4b172ab20d6edacd1d0a3fd9ac7bdc6e4c1d9958
정재현 <87860163+pastjung@users.noreply.github.com>
2026-02-18 12:43:55 +0900

    [D4] Title: [Professional] 키 순서, Time: 2,632 ms, Memory: 92,288 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.py
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.py
@@ -1,63 +1,39 @@
-# import sys
-# sys.stdin = open('input.txt')
-
-'''문제 분석
-- 문제 설명
-    - N명의 학생의 키 비교
-    - N명의 키는 모두 다름
-    - 일부 데이터만 존재 -> 일부 학생은 가장 작은 학생부터 몇 번째인지 알 수 있음
-        - a번 학생의 키가 b번 학생보다 작다면, a -> b
-    - 결론: 자신의 키가 몇 번째인지 알 수 있는 학생들이 몇 명인지 계산
-- 입력 데이터
-    - T : 테스트 케이스
-    - N : 학생들의 수
-    - M : 두 학생의 키를 비교한 횟수
-    - compares : 두 학생의 키 비교
-        - a b : a -> b (a < b)
-- 핵심 키워드
-    - 아이디어
-        - 나보다 키가 큰 사람 + 작은 사람 = 전체 인원 - 1
-'''
-
 def solve():
     N = int(input())
     M = int(input())
 
-    taller = [[] for _ in range(N + 1)]  # 본인보다 큰 학생 저장
-    smaller = [[] for _ in range(N + 1)] # 본인보다 작은 학생 저장
+    taller = [[] for _ in range(N + 1)]
+    smaller = [[] for _ in range(N + 1)]
 
+    # 자신보다 큰 사람/작은 사람 정보를 저장
     for _ in range(M):
         a, b = map(int, input().split())
         taller[a].append(b)
         smaller[b].append(a)
 
-    def cal_student(start_student, adj):
-        stack = [start_student]
-        visited = [False] * (N + 1)
+    # 자신보다 큰 사람/작은 사람을 계산하는 함수
+    def cal_student(start, adj):
+        for next in adj[start]:
+            if not visited[next]:
+                visited[next] = 1
+                cal_student(next, adj)
 
-        # 초기 세팅
-        visited[start_student] = True
-        count = 0
-
-        # 연결된 정보 확인
-        while stack:
-            cur_student = stack.pop()
-            for next_student in adj[cur_student]:
-                if not visited[next_student]:
-                    visited[next_student] = True
-                    count += 1
-                    stack.append(next_student)
-        
-        return count
-
+    # 자신보다 큰 사람 + 작은 사람 + 본인 = N
     result = 0
     for student in range(1, N + 1):
-        if cal_student(student, taller) + cal_student(student, smaller) == N - 1:
+        visited = [0] * (N + 1)
+
+        # 초기화
+        visited[student] = 1
+        
+        cal_student(student, taller)
+        cal_student(student, smaller)
+        
+        if sum(visited) == N:
             result += 1
 
     return result
-    
 
 T = int(input())
-for tc in range(1, T + 1):
+for tc in range(1, T+1):
     print(f'#{tc} {solve()}')
